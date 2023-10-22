697e7a9845152a3991f6fc55fb3ce2ec01ff1cb8
Marlon Velasquez <marlonjvelasquez@gmail.com>
2023-10-22 20:10:32 +0200

    Create a new directory views Create a new view MainView Rename componentes Change the structur of project

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,11 @@
 <template>
-  <div class="container-fluid">
-    <HeaderMain />
-    <SectionMain :imageText="imageText" />
+  <div>
+    <MainView />
   </div>
 </template>
 
 <script setup>
-import HeaderMain from "./components/headerMain.vue"
-import SectionMain from "./components/sectionMain.vue"
-
-import { ref } from "vue"
-
-const imageText = ref([
-  {
-    lineOne: "Catering",
-    lineTwo: "spagnolo",
-    mobile: "La_Bodeguita_del_sur_m1.jpg",
-    desktop: "La_Bodeguita_del_sur_d1.jpg",
-  },
-  {
-    lineOne: "Cooking",
-    lineTwo: "class",
-    mobile: "La_Bodeguita_del_sur_m2.jpg",
-    desktop: "La_Bodeguita_del_sur_d2.jpg",
-  },
-  {
-    lineOne: "Corsi di",
-    lineTwo: "spagnolo",
-    mobile: "La_Bodeguita_del_sur_m3.jpg",
-    desktop: "La_Bodeguita_del_sur_d3.jpg",
-  },
-])
+import MainView from "./views/MainView.vue"
 </script>
 
 <style lang="scss" scoped></style>
---
+++ src/views/MainView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container-fluid">
+    <HeaderMain />
+    <SectionMain :imageText="imageText" />
+  </div>
+</template>
+
+<script setup>
+import HeaderMain from "../components/MainHeader.vue"
+import SectionMain from "../components/MainSection.vue"
+
+import { ref } from "vue"
+
+const imageText = ref([
+  {
+    lineOne: "Catering",
+    lineTwo: "spagnolo",
+    mobile: "La_Bodeguita_del_sur_m1.jpg",
+    desktop: "La_Bodeguita_del_sur_d1.jpg",
+  },
+  {
+    lineOne: "Cooking",
+    lineTwo: "class",
+    mobile: "La_Bodeguita_del_sur_m2.jpg",
+    desktop: "La_Bodeguita_del_sur_d2.jpg",
+  },
+  {
+    lineOne: "Corsi di",
+    lineTwo: "spagnolo",
+    mobile: "La_Bodeguita_del_sur_m3.jpg",
+    desktop: "La_Bodeguita_del_sur_d3.jpg",
+  },
+])
+</script>
+
+<style lang="scss" scoped></style>
